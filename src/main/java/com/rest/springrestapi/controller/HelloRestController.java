bcfb27dde06a09f2b9a80bc3b0b20ca5f9d10214
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-05 15:58:43 +0530

    Added : POST Request Method And Pass First Name And Last Name In The Body

--- src/main/java/com/rest/springrestapi/controller/HelloRestController.java
+++ src/main/java/com/rest/springrestapi/controller/HelloRestController.java
@@ -4,6 +4,7 @@ package com.rest.springrestapi.controller;
  * Create a Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the User.
  */
 
+import com.rest.springrestapi.controller.entity.User;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -37,5 +38,19 @@ public class HelloRestController {
     @GetMapping(value= "/param/{name}")
     public String sayHelloParam(@PathVariable String name){
         return "Hello " + name + " Welcome To BridgeLabz";
+    }
+    /** @PostMapping : this annotation handle the post type of annotation
+     *  @RequestBody : is used to convert the body of HTTP request to java class object
+     *  @CURL : http://localhost:8080/post?Content-type=json : URL requests through POSTMAN
+     *  @JSON:{
+     *          "firstName":"Suraj",
+     *          "lastName" :"Chaudhary"
+     *      }
+     * @O/P ; Hello Suraj Chaudhary Welcome To BridgeLabz
+     */
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody User userInfo ){
+        return "Hello " + userInfo.getFirstName() +" "+userInfo.getLastName() + " Welcome To BridgeLabz";
+
     }
 }
